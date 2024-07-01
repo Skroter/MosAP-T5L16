c3cd0873901768a65f522a348d0941a158afb472
Skroter <digro@yandex.ru>
2024-07-01 17:29:07 +0500

    Добавьте файлы проекта.

---
+++ Topic5Lesson16Task1/Topic5Lesson16Task1.py
@@ -0,0 +1,23 @@
+﻿class boxOffice:
+    def __init__(self, money):
+        self.money = money
+ 
+    def top_up(self, x):
+        self.money += x
+        return f'Внесена сумма в кассу {x} рублей, в кассе сейчас {self.money} рублей.'
+    
+    def count_1000(self):
+        return f'В кассе сейчас {self.money // 1000} купюр(-ра/-ры) по тысяче рублей.'
+ 
+    def take_away(self, x):
+        if x <= self.money:
+            self.money -= x
+            return f'Из кассы выдано {x} рублей, в кассе осталось {self.money} рублей.'
+        else:
+            return f'Не достаточно денег.'
+        
+
+boxOffice1 = boxOffice(1000)
+print(boxOffice1.top_up(1000))
+print(boxOffice1.count_1000())
+print(boxOffice1.take_away(900))
---
+++ Topic5Lesson16Task2/Topic5Lesson16Task2.py
@@ -0,0 +1,33 @@
+﻿class turtle:
+    def __init__(self, x, y, s):
+        self.x = x
+        self.y = y
+        self.s = s
+    
+    def go_up(self): # увеличивает y на s
+        self.y += self.s
+    
+    def go_down(self): # уменьшает y на s
+        self.y -= self.s
+        
+    def go_left(self): # уменьшает x на s
+        self.x -= self.s
+        
+    def go_right(self): # увеличивает y на s
+        self.y += self.s
+        
+    def evolve(self): # увеличивает s на 1
+        self.s += 1
+
+    def degrade(self): # уменьшает s на 1 или выкидывает ошибку, когда s может стать ≤ 0
+        if self.s <= 0:
+            return f"Недопустимое значение условием. Error"
+        self.s -= 1
+        
+    def count_moves(self, x2, y2): # возвращает минимальное количество действий, за которое черепашка сможет добраться до x2 y2 от текущей позиции
+        if x2 % self.s == 0: 
+            return (x2 % self.s)
+        return (x2 % self.s + 1)
+        if y2 % self.s == 0: 
+            return (y2 % self.s)
+        return (y2 % self.s + 1)
